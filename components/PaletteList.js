444166d2562d27ac8ad7372fa7ee97bf0bc5b4f1
AdrianValdes <adrianvaldeshdz@gmail.com>
2021-04-15 11:59:14 +0200

    add color boxes

--- components/PaletteList.js
+++ components/PaletteList.js
@@ -1,6 +1,12 @@
 import { useNavigation } from '@react-navigation/core';
 import React from 'react';
-import { Text, TouchableOpacity, StyleSheet } from 'react-native';
+import {
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  FlatList,
+  View,
+} from 'react-native';
 
 export const PaletteList = ({ paletteName, colors }) => {
   const { navigate } = useNavigation();
@@ -14,6 +20,15 @@ export const PaletteList = ({ paletteName, colors }) => {
       }}
     >
       <Text style={styles.text}>{paletteName}</Text>
+      <FlatList
+        style={styles.list}
+        horizontal={true}
+        data={colors.slice(0, 5)}
+        keyExtractor={(item) => item.colorName}
+        renderItem={({ item: { colorName, hexCode } }) => (
+          <View style={[styles.box, { backgroundColor: hexCode }]} />
+        )}
+      />
     </TouchableOpacity>
   );
 };
@@ -22,5 +37,19 @@ const styles = StyleSheet.create({
   text: {
     fontWeight: 'bold',
     fontSize: 18,
+    marginBottom: 10,
+  },
+  box: {
+    height: 30,
+    width: 30,
+    marginRight: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 1,
+    elevation: 2,
+  },
+  list: {
+    marginBottom: 20,
   },
 });
