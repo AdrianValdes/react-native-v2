fa48d32e1cd5bfb7cb0e8b7c1c1f05df8120e3ad
AdrianValdes <adrianvaldeshdz@gmail.com>
2021-04-15 14:03:21 +0200

    add useEffect

--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, FlatList } from 'react-native';
-import { COLOR_PALETTES } from '../constants/COLORS';
+
 import { PaletteList } from '../components/PaletteList';
 
 export const Home = () => {
+  const [colorPalettes, setColorPalettes] = useState([]);
+
+  useEffect(() => {
+    const getColors = async () => {
+      try {
+        const result = await fetch(
+          'https://color-palette-api.kadikraman.now.sh/palettes',
+        );
+        const palettes = await result.json();
+        console.log(palettes);
+        setColorPalettes(palettes);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    getColors();
+  }, []);
   return (
     <View>
       <FlatList
-        data={COLOR_PALETTES}
+        data={colorPalettes}
         keyExtractor={(item) => item.paletteName}
         renderItem={({ item: { paletteName, colors } }) => (
           <PaletteList paletteName={paletteName} colors={colors} />
