ec548587eafed9facf826e9ec5a1c991dbda4962
AdrianValdes <adrianvaldeshdz@gmail.com>
2021-04-15 14:58:10 +0200

    add refreshing on pull

--- screens/Home.js
+++ screens/Home.js
@@ -1,27 +1,35 @@
 import React, { useEffect, useState } from 'react';
-import { View, FlatList } from 'react-native';
+import { View, FlatList, RefreshControl } from 'react-native';
 
 import { PaletteList } from '../components/PaletteList';
 
 export const Home = () => {
   const [colorPalettes, setColorPalettes] = useState([]);
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+  const getColors = async () => {
+    try {
+      setIsRefreshing(true);
+      console.log('refreshes');
+      const result = await fetch(
+        'https://color-palette-api.kadikraman.now.sh/palettes',
+      );
+      const palettes = await result.json();
+
+      setColorPalettes(palettes);
+      setIsRefreshing(false);
+    } catch (e) {
+      console.log(e);
+      setIsRefreshing(false);
+    }
+  };
+
+  const handleRefresh = () => getColors();
 
   useEffect(() => {
-    const getColors = async () => {
-      try {
-        const result = await fetch(
-          'https://color-palette-api.kadikraman.now.sh/palettes',
-        );
-        const palettes = await result.json();
-        console.log(palettes);
-        setColorPalettes(palettes);
-      } catch (e) {
-        console.log(e);
-      }
-    };
-
     getColors();
   }, []);
+
   return (
     <View>
       <FlatList
@@ -30,6 +38,11 @@ export const Home = () => {
         renderItem={({ item: { paletteName, colors } }) => (
           <PaletteList paletteName={paletteName} colors={colors} />
         )}
+        refreshing={isRefreshing}
+        onRefresh={handleRefresh}
+        // refreshControl={
+        //   <RefreshControl refreshing={true} onRefresh={() => {}} />
+        // }
       />
     </View>
   );
